d747b342e77b74795b0b429ec4a985898a75682d
mztikk <nevigskcuforez@gmail.com>
2020-01-18 13:43:10 +0100

    added tests

---
+++ RPipes.Test/PipeReaderTests/ReadLineTests.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.IO.Pipelines;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RPipes.Test.PipeReaderTests
+{
+    [TestClass]
+    public class ReadLineTests
+    {
+        private readonly string[] _files = new[] { "LoremIpsum100.txt", "LoremIpsum1000.txt", "LoremIpsum100000.txt" };
+        private readonly long[] _lengths = new long[] { 199, 1999, 199999 };
+
+        [TestMethod]
+        public async Task ReadLines()
+        {
+            for (int i = 0; i < _files.Length; i++)
+            {
+                using (FileStream fs = new FileStream(_files[i], FileMode.Open, FileAccess.Read))
+                {
+                    Pipe pipe = new Pipe();
+                    Task write = pipe.Writer.FillFrom(fs);
+                    int count = 0;
+                    await foreach (string item in pipe.Reader.ReadLines())
+                    {
+                        count++;
+                    }
+
+                    Assert.AreEqual(_lengths[i], count);
+                    await write.ConfigureAwait(false);
+                }
+            }
+        }
+    }
+}
